94b43ac978eda706d82df12a1592abe6cd92dca8
Luis Felipe Ramos <116222857+felipeeRamos@users.noreply.github.com>
2023-05-15 16:02:00 -0300

    comentario no codigo

--- ex08.c
+++ ex08.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 int main(){
-	float celcius,fahrenheit;
+	float celcius,fahrenheit;//Declaração de variaveis float
 	
-	printf("Digite o valor em Celcius:\n");
-	scanf("%f",&celcius);
+	printf("Digite o valor em Celcius:\n");//solicitação para entrada de dados
+	scanf("%f",&celcius);//valor que o usuario digitou
 	
-	fahrenheit = 9*celcius + 32;
+	fahrenheit = 9 * celcius + 32;//operação para conversão de celcius para fahrenheit
 	
-	printf("Valor em Fahrenheit eh %f",fahrenheit);
+	printf("Valor em Fahrenheit eh %f",fahrenheit);//resultado em fahrenheit
 	
-	
-	
-	
+	return  0;
 	
 }
